f96b392052fa6b869332b80ae477b33fc89810a9
falcom0 <f.ichwan@gmail.com>
2017-03-30 19:48:34 +0700

    Recommit from local pc

---
+++ application/views/templates_c/9bee1ac92d1778923b6c981e690ab3b2cf276fbb.file.footer.html.php
@@ -0,0 +1,25 @@
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2017-03-30 12:26:58
+         compiled from "D:\wamp64\www\gedung\application\views\templates\partials\footer.html" */ ?>
+<?php /*%%SmartyHeaderCode:799258dcf9922b1048-81489682%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '9bee1ac92d1778923b6c981e690ab3b2cf276fbb' => 
+    array (
+      0 => 'D:\\wamp64\\www\\gedung\\application\\views\\templates\\partials\\footer.html',
+      1 => 1459216418,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '799258dcf9922b1048-81489682',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.21-dev',
+  'unifunc' => 'content_58dcf9922b5c19_61187657',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_58dcf9922b5c19_61187657')) {function content_58dcf9922b5c19_61187657($_smarty_tpl) {?><footer class="footer-content">
+    2014 - <span id="copyright-year"></span> &copy; Blankon Admin. Created by <a href="http://djavaui.com/" target="_blank">Djava UI</a>, Yogyakarta ID
+    <span class="pull-right">0.01 GB(0%) of 15 GB used</span>
+</footer><!-- /.footer-content --><?php }} ?>
